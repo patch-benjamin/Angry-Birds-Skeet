d14aabf88d80d6dc75c73a4d756ca79b0c5b30fb
Benjamin Patch <benjaminpatch@Benjamins-MacBook-Air-3.local>
2014-10-23 20:59:23 -0600

    Initial Commit

---
+++ Skeet Good/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Skeet Good
+//
+//  Created by Benjamin Patch on 10/23/14.
+//  Copyright (c) 2014 Benjamin Patch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
