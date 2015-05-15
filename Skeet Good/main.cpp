92de39eda8e1e26d588bc6e72e0ac08edf2267f0
Benjamin Patch <pat10005@byui.edu>
2015-05-15 17:24:17 -0500

    Update main.cpp

--- Skeet Good/main.cpp
+++ Skeet Good/main.cpp
@@ -5,7 +5,7 @@
 //  Created by Benjamin Patch on 10/23/14.
 //  Copyright (c) 2014 Benjamin Patch. All rights reserved.
 //
-
+//Why is this main like this??
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
